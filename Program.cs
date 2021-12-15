09a64f5d315dd2ea3c0f87980e123896225f8f17
Donna Seidel <dseidel01@gmail.com>
2021-12-15 17:18:50 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace LongestWord
+{
+    class Program
+    {
+        public static String LongestWord(string sen)
+        {
+            // code goes here  
+            string[] words = sen.Split(" ");
+            string[] sorted = words
+                .OrderBy(x => x.Length)
+                .ToArray();
+            sen = sorted.ToString();
+            return sen;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your sentence and hit enter.");
+            string sen = Console.ReadLine();
+            LongestWord(sen);
+            string[] words = sen.Split(" ");
+            Console.WriteLine("The longest word is: " + words[0]);
+        }
+    }
+}
